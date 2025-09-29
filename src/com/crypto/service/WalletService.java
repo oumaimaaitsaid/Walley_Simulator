ffef398d1e60b725a4a879f2719f47e234090294
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2025-09-29 15:48:43 +0100

    CWS-3 Ajout du constructeur ServiceWallet pour initialiser le repository

--- src/com/crypto/service/WalletService.java
+++ src/com/crypto/service/WalletService.java
@@ -1,5 +1,33 @@
 package com.crypto.service;
 
-public class WalletService {
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import com.crypto.model.Wallet;
+import com.crypto.model.Enum.WalletType;
+import com.crypto.repository.IWalletRepository;
+import com.crypto.repository.WalletRepository;
+import com.crypto.utils.AddressGenerator;
+import com.crypto.utils.IdGenerator;
+
+
+
+public class WalletService  implements IWalletService {
+
+	private final IWalletRepository walletRepository;
+
+	public WalletService() throws SQLException {
+
+		this.walletRepository = new WalletRepository();
+	}
+
+
+	
+
+	
+
 
 }
