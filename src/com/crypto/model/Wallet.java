d9bbbd36456b17bc431f93515115d8a6c710261f
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2025-09-29 14:00:20 +0100

    CWS-1 ajouter Class Wallet avec getters/setters

--- src/com/crypto/model/Wallet.java
+++ src/com/crypto/model/Wallet.java
@@ -1,5 +1,36 @@
 package com.crypto.model;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import com.crypto.model.Enum.WalletType;
+
+
 public class Wallet {
 
+	private int id;
+	private UUID walletUuid ;
+	private WalletType type;
+	private String address;
+	private  double balance;
+	private LocalDateTime createdAt ;
+	
+
+	public int getId() {return id;}
+	public void setId(int id) {this.id=id;}
+	
+	public UUID getWalletUuid() {return walletUuid;}
+	public void setWalletUuid(UUID uuid) {this.walletUuid=uuid;}
+	
+	public WalletType getType() {return type;}
+	public void setType(WalletType type) {this.type=type;}
+	
+    public String getAddress() { return address; }
+	public void setAddress(String address) { this.address = address; }
+
+	public double getBalance() { return balance; }
+	public void setBalance(double balance) { this.balance = balance; }
+
+	public LocalDateTime getCreatedAt() { return createdAt; }
+	public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
 }
