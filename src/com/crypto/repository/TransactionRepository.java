e6d7d1a124a743f2ce69785fe13d9afd1e6d50aa
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2025-09-29 15:09:51 +0100

    CWS-2 Ajout de la méthode save() pour enregistrer une transaction dans la base de données

---
+++ src/com/crypto/repository/TransactionRepository.java
@@ -0,0 +1,62 @@
+package com.crypto.repository;
+
+import java.sql.*;
+import java.util.*;
+import com.crypto.model.Transaction;
+import com.crypto.model.Enum.Priority;
+import com.crypto.model.Enum.Status;
+import com.crypto.utils.DataBaseConnection;
+
+public class TransactionRepository implements ITransactionRepository {
+
+	private Connection connection;
+
+	public TransactionRepository() throws SQLException {
+
+		this.connection = DataBaseConnection.getConnection();
+	}
+
+	/**
+	 * Sauvegarder d'une transaction
+	 */
+	@Override
+
+	public Transaction save(Transaction transaction) {
+
+		String sql = "INSERT INTO transactions "
+				+ "(tx_uuid,wallet_id,source_address,destination_address,amount,fees,priority,status,created_at) "
+				+ "VALUES (?,?,?,?,?,?,?,?,?)";
+		try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+			stmt.setObject(1, transaction.getTxUuid());
+			stmt.setObject(2, transaction.getWalletId());
+			stmt.setString(3, transaction.getSourceAddress());
+			stmt.setString(4, transaction.getDestinationAddress());
+			stmt.setDouble(5, transaction.getAmount());
+			stmt.setDouble(6, transaction.getFees());
+			stmt.setString(7, transaction.getPriority().name());
+			stmt.setString(8, transaction.getStatus().name());
+			stmt.setTimestamp(9, Timestamp.valueOf(transaction.getCreatedAt()));
+
+			int rows = stmt.executeUpdate();
+			if (rows > 0) {
+				return transaction;
+			} else {
+				throw new SQLException("la transaction non enregistré");
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+
+		}
+
+	}
+
+
+
+	
+	
+	
+
+
+}
