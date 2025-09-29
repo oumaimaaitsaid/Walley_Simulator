a5d43e3229d3c82b695d2b4446f71b245d5a5a60
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2025-09-29 14:43:43 +0100

    CWS-1 implémenter la méthode save pour souvegardé un wallet en base de donnée

--- src/com/crypto/repository/WalletRepository.java
+++ src/com/crypto/repository/WalletRepository.java
@@ -16,7 +16,32 @@ public class WalletRepository implements IWalletRepository {
 		this.connection = DataBaseConnection.getConnection();
 	}
 
-	
+	/**
+	 * souvegardé un wallet en base de données
+	 */
+	@Override
+	public Wallet save(Wallet wallet) {
+		String sql = "INSERT INTO wallets (wallet_uuid, type, address, balance, created_at) VALUES (?,?,?,?,?)";
+		try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+			stmt.setObject(1, wallet.getWalletUuid());
+			stmt.setString(2, wallet.getType().name());
+			stmt.setString(3, wallet.getAddress());
+			stmt.setDouble(4, wallet.getBalance());
+			stmt.setTimestamp(5, Timestamp.valueOf(wallet.getCreatedAt()));
+
+			int rows = stmt.executeUpdate();
+			if (rows > 0) {
+				return wallet;
+			} else {
+				throw new SQLException("échec de enregistré du wallet");
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
 	
 	
 
