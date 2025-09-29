5096f52d1a62d86654350af5ca0243e42f7ef81f
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2025-09-29 16:00:45 +0100

    CWS-3 Ajout du constructeur TransactionService pour initialiser les repositories

--- src/com/crypto/service/TransactionService.java
+++ src/com/crypto/service/TransactionService.java
@@ -1,5 +1,30 @@
 package com.crypto.service;
 
-public class TransactionService {
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import com.crypto.model.Transaction;
+import com.crypto.model.Enum.Priority;
+import com.crypto.model.Enum.Status;
+import com.crypto.repository.ITransactionRepository;
+import com.crypto.repository.TransactionRepository;
+import com.crypto.repository.WalletRepository;
+import com.crypto.utils.IdGenerator;
+
+public class TransactionService implements ITransactionsService {
+
+	private final ITransactionRepository TransactionRepository;
+	private final WalletRepository walletRepository;
+
+	public TransactionService() throws SQLException {
+		this.TransactionRepository = new TransactionRepository();
+		this.walletRepository = new WalletRepository();
+	}
+
+	
+	
+
 
 }
